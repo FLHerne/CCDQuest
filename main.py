5fd6d5d6b26c82a9bbc0c453666922617caceb02
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-01 14:35:41 +0100

    random, non-change-induced worrying

--- main.py
+++ main.py
@@ -341,6 +341,8 @@ def updateContextMessages(x, y, currentMessage):
             currentMessage = "Is this " + str(RealMap[x, y].name) + " safe?"
         else:
             currentMessage = "You reach some " + str(RealMap[x, y].name)
+    if (random.randint(0, 200) < RealMap[x, y].difficulty):
+        currentMessage = "Is this " + str(RealMap[x, y].name) + " really safe?"
     return currentMessage
     
     
