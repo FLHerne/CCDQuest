2576f638cd4ae53dce035c3f0615a31cbd255df3
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-02 22:25:12 +0100

    font now a variable

--- newTextBox.py
+++ newTextBox.py
@@ -3,10 +3,11 @@ from colours import *
 
 defaultSize = 14
 defaultColour = WHITE
+defaultFont = 'Monospace'
 
 def Draw(drawSurface, textString, rect, colour=defaultColour, centred=True):
     size = defaultSize
-    font = pygame.font.SysFont('Monospace', defaultSize)
+    font = pygame.font.SysFont(defaultFont, defaultSize)
     textBitmap = font.render(textString, True, colour)
     if centred:
         offset = ((rect.width - textBitmap.get_width())/2, (rect.height - textBitmap.get_height())/2)
