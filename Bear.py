6b02e1b6481f15a0da29a91c0173baaedfc521ea
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-06 00:11:17 +0100

    Create new file for Bear.
    Several bear-related functions still in main.py.

---
+++ Bear.py
@@ -0,0 +1,60 @@
+import images
+import random
+
+class Bear:
+    '''follows you around when in range'''
+    def __init__(self, position):
+        '''setup bear in given position'''
+        self.position = list(position)
+        self.direction = -1 # Left
+
+    def huntplayer(self, playerpos, cellmap):
+        '''move towards the player'''
+        if abs(playerpos[0]-self.position[0]) + abs(playerpos[1]-self.position[1]) > 15:
+            return False
+        def mapcoord(d_coord):
+            return (self.position[0] + d_coord[0] - 32,
+                    self.position[1] + d_coord[1] - 32)
+        def istarget(d_coord):
+            return (mapcoord(d_coord)[0]%cellmap.size[0] == playerpos[0]%cellmap.size[0] and
+                    mapcoord(d_coord)[1]%cellmap.size[1] == playerpos[1]%cellmap.size[1])
+
+        foundtarget = False
+        dijkstramap = [[(512, (32, 32)) for x in xrange(64)] for x in xrange(64)]
+        import heapq
+        openlist = []
+        heapq.heappush(openlist, (0, (32, 32)))
+        curp = False
+        while openlist:
+            curn = heapq.heappop(openlist)
+            curd = curn[0]
+            curp = curn[1]
+            #print "CurP is", curp
+            if istarget(curp):
+                foundtarget = True
+                break
+            for nbrpos in [(curp[0]-1, curp[1]), (curp[0], curp[1]-1), (curp[0]+1, curp[1]), (curp[0], curp[1]+1)]:
+                if nbrpos[0] < 0 or nbrpos[1] < 0 or nbrpos[0] >= 64 or nbrpos[1] >= 64:
+                    continue
+                if dijkstramap[nbrpos[0]][nbrpos[1]][0] != 512 or cellmap[mapcoord(nbrpos)].solid:
+                    continue
+                dijkstramap[nbrpos[0]][nbrpos[1]] = (curd+1, curp)
+                heapq.heappush(openlist, (curd+1, nbrpos))
+        if not foundtarget:
+            print "Bear pathfinder failed"
+            return False
+        print "Bear pathfinder succeeded"
+        while dijkstramap[curp[0]][curp[1]][1] != (32, 32):
+            curp = dijkstramap[curp[0]][curp[1]][1]
+        self.position[0] += curp[0]-32
+        self.direction = curp[0]-32 if abs(curp[0]-32) else self.direction
+        self.position[1] += curp[1]-32
+        return True
+    
+    def draw(self, drawSurface, cellmap):
+        '''Blit self to specified surface'''
+        if cellmap[self.position].top or not cellmap[self.position].visible:
+            return
+        x = ((self.position[0]*images.BLOCKSIZE))
+        y = ((self.position[1]*images.BLOCKSIZE))
+        drawSurface.blit(images.BearRight if self.direction > 0 else images.BearLeft, (x, y))
--- main.py
+++ main.py
@@ -13,8 +13,9 @@ import TextBox
 import newTextBox
 import random
 import images
+from Bear import Bear
 from Cell import Cell
-import Map
+from Map import Map
 
 from colours import *
 
@@ -72,7 +73,7 @@ else:                               # mode using WASD
 
 PLAYER1 = MAGENTA
 
-cellmap = Map.Map(groundFile, collectablesFile)
+cellmap = Map(groundFile, collectablesFile)
 worldSize = cellmap.size
 totalCoins = cellmap.origcoins
 Pos = list(cellmap.startpos)
@@ -146,70 +147,14 @@ def CrossCheck():
         cellmap[Pos[0], (Y*i)+Pos[1]].visible = True
 
 # -----------------------------------------------------------------------------
-        
-class Bear:
-    '''follows you around when in range'''
-    def __init__(self, position):
-        '''setup bear in given position'''
-        self.position = list(position)
-        self.direction = -1 # Left
-    def hunt(self):
-        '''move towards the player'''
-        if abs(Pos[0]-self.position[0]) + abs(Pos[1]-self.position[1]) > 15:
-            return False
-        def worldPos(d_coord):
-            return (self.position[0] + d_coord[0] - 32,
-                    self.position[1] + d_coord[1] - 32)
-        def isTarget(d_coord):
-            return (worldPos(d_coord)[0]%worldSize[0] == Pos[0]%worldSize[0] and
-                    worldPos(d_coord)[1]%worldSize[1] == Pos[1]%worldSize[1])
 
-        foundtarget = False
-        dijkstramap = [[(512, (32, 32)) for x in xrange(64)] for x in xrange(64)]
-        import heapq
-        openlist = []
-        heapq.heappush(openlist, (0, (32, 32)))
-        curp = False
-        while openlist:
-            curn = heapq.heappop(openlist)
-            curd = curn[0]
-            curp = curn[1]
-            #print "CurP is", curp
-            if isTarget(curp):
-                foundtarget = True
-                break
-            for nbrpos in [(curp[0]-1, curp[1]), (curp[0], curp[1]-1), (curp[0]+1, curp[1]), (curp[0], curp[1]+1)]:
-                if nbrpos[0] < 0 or nbrpos[1] < 0 or nbrpos[0] >= 64 or nbrpos[1] >= 64:
-                    continue
-                if dijkstramap[nbrpos[0]][nbrpos[1]][0] != 512 or cellmap[worldPos(nbrpos)].solid:
-                    continue
-                dijkstramap[nbrpos[0]][nbrpos[1]] = (curd+1, curp)
-                heapq.heappush(openlist, (curd+1, nbrpos))
-        if not foundtarget:
-            DebugPrint("Bear pathfinder failed")
-            return False
-        DebugPrint("Bear pathfinder succeeded")
-        while dijkstramap[curp[0]][curp[1]][1] != (32, 32):
-            curp = dijkstramap[curp[0]][curp[1]][1]
-        self.position[0] += curp[0]-32
-        self.direction = curp[0]-32 if abs(curp[0]-32) else self.direction
-        self.position[1] += curp[1]-32
-        return True
-    
-    def draw(self, drawSurface):
-        '''Blit self to specified surface'''
-        if cellmap[self.position].top or not cellmap[self.position].visible:
-            return
-        x = ((self.position[0]*images.BLOCKSIZE))
-        y = ((self.position[1]*images.BLOCKSIZE))
-        drawSurface.blit(images.BearRight if self.direction > 0 else images.BearLeft, (x, y))
 
 def placeBears(number):
     '''randomly add bears to the map'''
     max_attempts = 20*number
     created = []
     for i in xrange(max_attempts):
-        attempt = (random.randint(0,worldSize[0]-1), random.randint(0,worldSize[1]-1))
+        attempt = (random.randint(0,cellmap.size[0]-1), random.randint(0,cellmap.size[1]-1))
         if cellmap[attempt].name not in ['grass', 'forest', 'rocky ground']:
             continue
         created.append(Bear(attempt))
@@ -220,14 +165,14 @@ def placeBears(number):
 def moveBears(bearlist):
     '''make each bear move towards the player using a pathfinder'''
     for bear in bearlist:
-        bear.hunt()
+        bear.huntplayer(Pos, cellmap)
 
 def drawBears(bearlist, drawSurface):
     '''call the draw function for each bear'''
     for bear in bearlist:
-        bear.draw(drawSurface)
+        bear.draw(drawSurface, cellmap)
 
-bears = placeBears(int(worldSize[0]*worldSize[1]/5000))
+bears = placeBears(int(cellmap.size[0]*cellmap.size[1]/5000))
 
 # -----------------------------------------------------------------------------
 
