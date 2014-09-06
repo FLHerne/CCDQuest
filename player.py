e7f2e958aea899098d3cb254e660a6b5b9fa4343
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-06 11:25:38 +0100

    copied bits in

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ def player:
         self.position = position
         self.colour = colour
         self.visibility = visibility
+        self.visibletiles = []
     def draw(drawsurface):
         '''draw the player as a blinking circle'''
         if RealMap[Pos[0], Pos[1]].top == False:
@@ -12,4 +13,12 @@ def player:
             y = ((self.position[1]*BLOCKSIZE)+int(BLOCKSIZE/2))
             radius = int(BLOCKSIZE/2)
             pygame.draw.circle(drawSurface, MAGEN, (x%world.get_width(), y%world.get_height()), radius)
-            
+    def UpdateVisible():
+        '''update the visibility of cells by the player'''
+        for x in range(Pos[0]-VISIBILITY-1, Pos[0]+VISIBILITY+2):
+            for y in range(Pos[1]-VISIBILITY-1, Pos[1]+VISIBILITY+2):
+                RealMap[x, y].visible = False
+        CrossCheck()
+        DiagonalCheck()
+        for tile in visible_tiles(player1.pos, 15, map):
+            map[tile].visible = True map[tile].explored = True'
