d14ee4b169483424a6e4c7ea8f852facef87a096
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-06 23:59:05 +0100

    Move scrolling code out of main.py.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import time
 import TextBox
 from HUD import HUD
 from World import World
+from WorldView import WorldView
 
 from colours import *
 from images import *
@@ -63,16 +64,6 @@ def HandleEvents():
                 quitting = True
     return quitting
 
-def wrapCoords(scrollpos):
-    '''allow world.player to walk around the world in a loop'''
-    world.playerx = (world.player.position[0]*BLOCKSIZE)+scrollpos[0]
-    world.playery = (world.player.position[1]*BLOCKSIZE)+scrollpos[1]
-    if world.player.position[0] % world.cellmap.size[0] == int(world.cellmap.size[0]/2):
-        world.player.position[0] %= world.cellmap.size[0]
-    if world.player.position[1] % world.cellmap.size[1] == int(world.cellmap.size[1]/2):
-        world.player.position[1] %= world.cellmap.size[1]
-    return ((-BLOCKSIZE*world.player.position[0])+world.playerx, (-BLOCKSIZE*world.player.position[1])+world.playery)
-    
 def setup():
     '''to be used at the beginning of the programme'''
     pass
@@ -81,38 +72,7 @@ def loop():
     '''to be used repeatedly'''
     pass
 
-def mapWorldToScreen(scrollpos):
-    '''show part of the world on screen'''
-    old_clip = window.get_clip()
-    worldRegion = pygame.Rect(0, 0, windowSize[0]-90, windowSize[1]-20)
-    window.set_clip(worldRegion)
-    for tx in [scrollpos[0]-world.surface.get_width(), scrollpos[0], scrollpos[0]+world.surface.get_width()]:
-        for ty in [scrollpos[1]-world.surface.get_height(), scrollpos[1], scrollpos[1]+world.surface.get_height()]:
-            if world.surface.get_rect(topleft=(tx, ty)).colliderect(worldRegion):
-                window.blit(world.surface, (tx, ty))
-    window.set_clip(old_clip)
-
-def calculateScrollPos(scrollpos):
-    '''scroll towards the correct position'''
-    world.playerx = (world.player.position[0]*BLOCKSIZE)+scrollpos[0]
-    world.playery = (world.player.position[1]*BLOCKSIZE)+scrollpos[1]
-    if world.playerx+(world.player.visibility*BLOCKSIZE) > windowSize[0]-100:         #too far right
-        scrollStep = (abs((world.playerx+(world.player.visibility*BLOCKSIZE)) - (windowSize[0]-100)) / 2) +1
-        scrollpos = (scrollpos[0]-scrollStep, scrollpos[1])
-    if world.playerx-(world.player.visibility*BLOCKSIZE) < 0:                         #too far left
-        scrollStep = (abs((world.playerx-(world.player.visibility*BLOCKSIZE))) / 2) +1
-        scrollpos = (scrollpos[0]+scrollStep, scrollpos[1])
-    if world.playery+(world.player.visibility*BLOCKSIZE) > windowSize[1]:             #too far down
-        scrollStep = (abs((world.playery+(world.player.visibility*BLOCKSIZE)) - windowSize[1]) / 2) +1
-        scrollpos = (scrollpos[0], scrollpos[1]-scrollStep)
-    if world.playery-(world.player.visibility*BLOCKSIZE) < 0:                         #too far up
-        scrollStep = (abs((world.playery-(world.player.visibility*BLOCKSIZE))) / 2) +1
-        scrollpos = (scrollpos[0], scrollpos[1]+scrollStep)
-
-    return scrollpos
-    
-scrollpos = ((-BLOCKSIZE*world.player.position[0])+((windowSize[0]-100)/2), (-BLOCKSIZE*world.player.position[1])+(windowSize[1]/2))
-
+worldview = WorldView(world, (0, 0, windowSize[0]-100, windowSize[1]-20), window)
 hud = HUD(world, (windowSize[0]-100, 0, 100, windowSize[1]), window)
 
 quitting = False
@@ -120,9 +80,7 @@ while not quitting:
     time.sleep(0.04)
     quitting = HandleEvents()
 
-    scrollpos = calculateScrollPos(scrollpos)
-    mapWorldToScreen(scrollpos)
-    scrollpos = wrapCoords(scrollpos)
+    scrollpos = worldview.draw(world, window)
     hud.draw(world, window, scrollpos)
 
     pygame.display.update()
