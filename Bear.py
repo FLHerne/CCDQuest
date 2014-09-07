8c7f99630dd949a617c543cf7496e13fdad10f3c
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-07 22:45:36 +0100

    Revert "death to magic numbers"
    This reverts commit f5f3c0512a3a2f9d91155b24b15254f15d81cb5e.

--- Bear.py
+++ Bear.py
@@ -8,7 +8,6 @@ class Bear:
         self.position = list(position)
         self.direction = -1 # Left
         self.circleradius = 15
-        self.huntingrange = 32
 
     def huntplayer(self, playerpos, cellmap):
         '''move towards the player'''
@@ -17,17 +16,17 @@ class Bear:
         if (playerpos[0]-self.position[0])**2 + (playerpos[1]-self.position[1])**2 > self.circleradius**2:
             return False
         def mapcoord(d_coord):
-            return (self.position[0] + d_coord[0] - self.huntingrange,
-                    self.position[1] + d_coord[1] - self.huntingrange)
+            return (self.position[0] + d_coord[0] - 32,
+                    self.position[1] + d_coord[1] - 32)
         def istarget(d_coord):
             return (mapcoord(d_coord)[0]%cellmap.size[0] == playerpos[0]%cellmap.size[0] and
                     mapcoord(d_coord)[1]%cellmap.size[1] == playerpos[1]%cellmap.size[1])
 
         foundtarget = False
-        dijkstramap = [[(512, (self.huntingrange, self.huntingrange)) for x in xrange(2*self.huntingrange)] for x in xrange(2*self.huntingrange)]
+        dijkstramap = [[(512, (32, 32)) for x in xrange(64)] for x in xrange(64)]
         import heapq
         openlist = []
-        heapq.heappush(openlist, (0, (self.huntingrange, self.huntingrange)))
+        heapq.heappush(openlist, (0, (32, 32)))
         curp = False
         while openlist:
             curn = heapq.heappop(openlist)
@@ -38,7 +37,7 @@ class Bear:
                 foundtarget = True
                 break
             for nbrpos in [(curp[0]-1, curp[1]), (curp[0], curp[1]-1), (curp[0]+1, curp[1]), (curp[0], curp[1]+1)]:
-                if nbrpos[0] < 0 or nbrpos[1] < 0 or nbrpos[0] >= 2*self.huntingrange or nbrpos[1] >= 2*self.huntingrange:
+                if nbrpos[0] < 0 or nbrpos[1] < 0 or nbrpos[0] >= 64 or nbrpos[1] >= 64:
                     continue
                 if dijkstramap[nbrpos[0]][nbrpos[1]][0] <=curd+cellmap[mapcoord(nbrpos)].difficulty or cellmap[mapcoord(nbrpos)].solid:
                     continue
@@ -46,11 +45,11 @@ class Bear:
                 heapq.heappush(openlist, (curd+cellmap[mapcoord(nbrpos)].difficulty, nbrpos))
         if not foundtarget:
             return False
-        while dijkstramap[curp[0]][curp[1]][1] != (self.huntingrange, self.huntingrange):
+        while dijkstramap[curp[0]][curp[1]][1] != (32, 32):
             curp = dijkstramap[curp[0]][curp[1]][1]
-        self.position[0] += curp[0]-self.huntingrange
-        self.direction = curp[0]-self.huntingrange if abs(curp[0]-self.huntingrange) else self.direction
-        self.position[1] += curp[1]-self.huntingrange
+        self.position[0] += curp[0]-32
+        self.direction = curp[0]-32 if abs(curp[0]-32) else self.direction
+        self.position[1] += curp[1]-32
         return True
     
     def sprite(self):
