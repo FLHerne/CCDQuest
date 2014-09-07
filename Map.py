3fa83f3741676bfc15c1e3aea701041d678f78bf
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-07 16:39:26 +0100

    more convention-compliance

--- Map.py
+++ Map.py
@@ -3,10 +3,11 @@ from Cell import *
 from colors import *
 import collectables
 
+
 class Map():
     def index(self, coord):
-        return (int(coord[0]%self.size[0]*self.size[0]) + coord[1]%self.size[1])
-    
+        return (int(coord[0] % self.size[0] * self.size[0]) + coord[1] % self.size[1])
+
     def __init__(self, groundfile, collectablefile):
         START = MAGENTA
         groundimage = pygame.image.load(groundfile)
@@ -16,14 +17,14 @@ class Map():
         self.size = list(groundimage.get_rect().size)
         self.startpos = None
         self.origcoins = 0
-        
+
         self.cellarray = []
         for x in xrange(0, self.size[0]):
             for y in xrange(0, self.size[1]):
                 groundcolour = groundimage.unmap_rgb(groundmap[x, y])
                 collectablecolour = collectablesimage.unmap_rgb(collectablesmap[x, y])
                 self.cellarray.append(Cell(groundcolour, collectablecolour))
-                if collectablecolour == START: 
+                if collectablecolour == START:
                     self.startpos = (x, y)
                 elif self[x, y].collectableitem == collectables.COIN:
                     self.origcoins += 1
@@ -31,6 +32,6 @@ class Map():
 
     def __getitem__(self, coord):
         return self.cellarray[self.index(coord)]
-        
+
     def __setitem__(self, coord, value):
         self.cellarray[self.index(coord)] = value
