b2090d301ade1bb1130e31ac119cb15e287ea441
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-08 11:33:40 +0100

    Add FREEPLAYER constant for testing purposes.

--- Player.py
+++ Player.py
@@ -2,7 +2,8 @@ import pygame
 from colors import *
 from images import BLOCKSIZE
 import collectables
-
+# Remove solidity and movement cost for testing
+FREEPLAYER = False
 
 class Player:
     def __init__(self, position):
@@ -23,7 +24,7 @@ class Player:
 
     def move(self, x, y, cellmap):
         assert abs(x) + abs(y) <= 1
-        if cellmap[self.position[0]+x, self.position[1]+y].solid:
+        if cellmap[self.position[0]+x, self.position[1]+y].solid and not FREEPLAYER:
             self.score[collectables.CHOCOLATE] -= 50
             return False
         self.position = [(self.position[0]+x)%cellmap.size[0],
@@ -32,7 +33,8 @@ class Player:
         if collectable != None:
             self.score[collectable] += collectables.value[collectable]
         cellmap[self.position].collectableitem = None
-        self.score[collectables.CHOCOLATE] -= cellmap[self.position].difficulty
+        if not FREEPLAYER:
+            self.score[collectables.CHOCOLATE] -= cellmap[self.position].difficulty
         return True
 
     def sprite(self):
