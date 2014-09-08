e43eb5e26836ea9b4a973e489d768e535081114c
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-08 16:31:37 +0100

    Simplify WorldView.updatescrollpos()

--- WorldView.py
+++ WorldView.py
@@ -4,28 +4,16 @@ import pygame
 class WorldView:
     def __init__(self, world, area, window):
         self.area = pygame.Rect(area)
-        self.scrollpos = ((-BLOCKSIZE*world.player.position[0])+self.area.width/2,
-                          (-BLOCKSIZE*world.player.position[1])+self.area.height/2)
+        self.scrollpos = [(-BLOCKSIZE*world.player.position[0])+self.area.width/2,
+                          (-BLOCKSIZE*world.player.position[1])+self.area.height/2]
 
     def draw(self, world, window):
         def updatescrollpos():
             '''scroll towards the correct position'''
-            world.playerx = (world.player.position[0]*BLOCKSIZE + self.scrollpos[0]) % world.surface.get_width()
-            world.playery = (world.player.position[1]*BLOCKSIZE + self.scrollpos[1]) % world.surface.get_height()
-            if world.playerx+(world.player.visibility*BLOCKSIZE) > self.area.width:         #too far right
-                scrollStep = (abs((world.playerx+(world.player.visibility*BLOCKSIZE)) - (self.area.width)) / 2) +1
-                self.scrollpos = (self.scrollpos[0]-scrollStep, self.scrollpos[1])
-            if world.playerx-(world.player.visibility*BLOCKSIZE) < 0:                         #too far left
-                scrollStep = (abs((world.playerx-(world.player.visibility*BLOCKSIZE))) / 2) +1
-                self.scrollpos = (self.scrollpos[0]+scrollStep, self.scrollpos[1])
-            if world.playery+(world.player.visibility*BLOCKSIZE) > self.area.height:             #too far down
-                scrollStep = (abs((world.playery+(world.player.visibility*BLOCKSIZE)) - self.area.height) / 2) +1
-                self.scrollpos = (self.scrollpos[0], self.scrollpos[1]-scrollStep)
-            if world.playery-(world.player.visibility*BLOCKSIZE) < 0:                         #too far up
-                scrollStep = (abs((world.playery-(world.player.visibility*BLOCKSIZE))) / 2) +1
-                self.scrollpos = (self.scrollpos[0], self.scrollpos[1]+scrollStep)
-            self.scrollpos = (self.scrollpos[0] % world.surface.get_width(),
-                              self.scrollpos[1] % world.surface.get_height())
+            for axis in [0, 1]:
+                pq = (world.player.position[axis]*BLOCKSIZE+self.scrollpos[axis]) % world.surface.get_size()[axis]
+                dr = (world.player.visibility*BLOCKSIZE, self.area.size[axis]-world.player.visibility*BLOCKSIZE)
+                self.scrollpos[axis] = (self.scrollpos[axis]+(max(dr[0],min(dr[1],pq))-pq)/2) % world.surface.get_size()[axis]
 
         def blitworld():
             oldclip = window.get_clip()
