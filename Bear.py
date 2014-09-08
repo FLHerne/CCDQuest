fcde84cef625668efe9c4b794d5227ff2d7d5651
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-08 22:03:04 +0100

    Remove function 'Bear::huntplayer()::istarget()'
    Previous changes just made it a tuple equality test.

--- Bear.py
+++ Bear.py
@@ -19,11 +19,9 @@ class Bear:
             mindist(playerpos[1], self.position[1], cellmap.size[1])) > 15:
             return False
         def mapcoord(d_coord):
+            '''get map coordinate from pathfinder ones'''
             return ((self.position[0] + d_coord[0] - 32) % cellmap.size[0],
                     (self.position[1] + d_coord[1] - 32) % cellmap.size[1])
-        def istarget(d_coord):
-            return (mapcoord(d_coord)[0] == playerpos[0] and
-                    mapcoord(d_coord)[1] == playerpos[1])
 
         foundtarget = False
         dijkstramap = [[(512, (32, 32)) for x in xrange(64)] for x in xrange(64)]
@@ -35,7 +33,7 @@ class Bear:
             curn = heapq.heappop(openlist)
             curd = curn[0]
             curp = curn[1]
-            if istarget(curp):
+            if mapcoord(curp) == tuple(playerpos):
                 foundtarget = True
                 break
             for nbrpos in [(curp[0]-1, curp[1]), (curp[0], curp[1]-1), (curp[0]+1, curp[1]), (curp[0], curp[1]+1)]:
