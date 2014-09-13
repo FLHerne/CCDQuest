7e2ed882223d1a696adff82d99cdff57980120a7
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-13 13:56:18 +0100

    textbox bevleling is now less buggy, less elegant, less confusing

--- newTextBox.py
+++ newTextBox.py
@@ -9,18 +9,29 @@ def Draw(drawSurface, textString, rect, colour=defaultColour, size=defaultSize, 
     outputBitmap = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA, 32)
     font = pygame.font.Font(font, size)
     darkcolour = BLACK
-    maincolour = YELLOW
     lightcolour = DARKYELLOW
-    for x3doffset, y3doffset, colour in ((1, 1, darkcolour), (-1, -1, lightcolour), (0, 0, maincolour) if beveled else (0, 0, maincolour)):
-        xoffset = x3doffset
-        yoffset = y3doffset + rect.height-size                   # Clunky - FIXME
-        textBitmap = font.render(textString, True, colour)
-        if xcentred:
-            xoffset = x3doffset + (rect.width - textBitmap.get_width())/2
-        if ycentred:
-            yoffset = y3doffset + (rect.height - textBitmap.get_height())/2
+    if beveled:
+        for (x3doffset, y3doffset, bevelcolour) in (1, 1, darkcolour), (-1, -1, lightcolour):
+            xoffset = x3doffset
+            yoffset = y3doffset + rect.height-size                   # Clunky - FIXME
+            textBitmap = font.render(textString, True, bevelcolour)
+            if xcentred:
+                xoffset = x3doffset + (rect.width - textBitmap.get_width())/2
+            if ycentred:
+                yoffset = y3doffset + (rect.height - textBitmap.get_height())/2
+            
+            outputBitmap.blit(textBitmap, (xoffset, yoffset))
         
-        outputBitmap.blit(textBitmap, (xoffset, yoffset))
+    (x3doffset, y3doffset, colour) = (0, 0, colour)
+    xoffset = x3doffset
+    yoffset = y3doffset + rect.height-size                   # Clunky - FIXME
+    textBitmap = font.render(textString, True, colour)
+    if xcentred:
+        xoffset = x3doffset + (rect.width - textBitmap.get_width())/2
+    if ycentred:
+        yoffset = y3doffset + (rect.height - textBitmap.get_height())/2
+    
+    outputBitmap.blit(textBitmap, (xoffset, yoffset))
 
     drawSurface.blit(outputBitmap, rect.topleft)
     
