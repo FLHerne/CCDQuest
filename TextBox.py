6112c2474ad64757593f3b6bdf42af1006d176d3
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-13 15:11:32 +0100

    case standardisation

--- TextBox.py
+++ TextBox.py
@@ -1,9 +1,9 @@
 import pygame
 from colors import *
 
-defaultFont = "./fonts/MorrisRomanBlack.ttf" #'Dejavu Sans'
+defaultfont = "./fonts/MorrisRomanBlack.ttf" #'Dejavu Sans'
 
-def draw(drawSurface, textString, region, size, color, font=defaultFont, xcentered=True, ycentered=True, beveled=False):
+def draw(drawSurface, textstring, region, size, color, font=defaultfont, xcentered=True, ycentered=True, beveled=False):
     outputBitmap = pygame.Surface((region.width, region.height), pygame.SRCALPHA, 32)
     font = pygame.font.Font(font, size)
     maincolor = pygame.Color(color[0], color[1], color[2])
@@ -12,7 +12,7 @@ def draw(drawSurface, textString, region, size, color, font=defaultFont, xcenter
     for (xbeveloffset, ybeveloffset, color) in ((1, 1, darkcolor), (-1, -1, lightcolor), (0, 0, maincolor)) if beveled else ((0, 0, color),):
         xoffset = xbeveloffset
         yoffset = ybeveloffset + region.height-size                   # Clunky - FIXME
-        textBitmap = font.render(textString, True, color)
+        textBitmap = font.render(textstring, True, color)
         if xcentered:
             xoffset = xbeveloffset + (region.width - textBitmap.get_width())/2
         if ycentered:
