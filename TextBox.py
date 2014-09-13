620a7952282014493ff503ca049d8adcf5c73306
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-13 15:22:11 +0100

    appearance tweaks, renaming

--- TextBox.py
+++ TextBox.py
@@ -3,22 +3,22 @@ from colors import *
 
 defaultfont = "./fonts/MorrisRomanBlack.ttf" #'Dejavu Sans'
 
-def draw(drawSurface, string, region, size, color, font=defaultfont, xcentered=True, ycentered=True, beveled=False):
-    outputBitmap = pygame.Surface((region.width, region.height), pygame.SRCALPHA, 32)
+def draw(drawsurface, string, region, size, color, font=defaultfont, xcentered=True, ycentered=True, beveled=False):
+    outputbitmap = pygame.Surface((region.width, region.height), pygame.SRCALPHA, 32)
     font = pygame.font.Font(font, size)
     maincolor = pygame.Color(color[0], color[1], color[2])
     darkcolor = BLACK # maincolor.correct_gamma(10)
-    lightcolor = WHITE # maincolor.correct_gamma(0.1)
-    for (xbeveloffset, ybeveloffset, color) in ((1, 1, darkcolor), (-1, -1, lightcolor), (0, 0, maincolor)) if beveled else ((0, 0, color),):
+    lightcolor = BLACK # maincolor.correct_gamma(0.1)
+    for (xbeveloffset, ybeveloffset, color) in ((1, 1, darkcolor), (1, -1, lightcolor), (-1, 1, darkcolor), (-1, -1, lightcolor), (0, 0, maincolor)) if beveled else ((0, 0, color),):
         xoffset = xbeveloffset
         yoffset = ybeveloffset + region.height-size                   # Clunky - FIXME
-        textBitmap = font.render(string, True, color)
+        textbitmap = font.render(string, True, color)
         if xcentered:
-            xoffset = xbeveloffset + (region.width - textBitmap.get_width())/2
+            xoffset = xbeveloffset + (region.width - textbitmap.get_width())/2
         if ycentered:
-            yoffset = ybeveloffset + (region.height - textBitmap.get_height())/2
+            yoffset = ybeveloffset + (region.height - textbitmap.get_height())/2
         
-        outputBitmap.blit(textBitmap, (xoffset, yoffset))
+        outputbitmap.blit(textbitmap, (xoffset, yoffset))
 
-    drawSurface.blit(outputBitmap, region.topleft)
+    drawsurface.blit(outputbitmap, region.topleft)
     
