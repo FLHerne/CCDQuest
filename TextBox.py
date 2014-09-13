b6df34f34dc0537376fecc2c444b5ed6631331a2
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-13 14:54:15 +0100

    merged newtextbox and textbox, due to confusion during merge

--- TextBox.py
+++ TextBox.py
@@ -1,20 +1,26 @@
 import pygame
 from colors import *
 
-def draw(drawsurface, string, region, colour=YELLOW, size=12, font="./fonts/MorrisRomanBlack.ttf", xcentered=True, ycentered=True, beveled=False):
-    outputbitmap = pygame.Surface(region.size, pygame.SRCALPHA, 32)
-    font = pygame.font.Font(font, size)
-    darkcolour = BLACK
-    maincolour = YELLOW
-    lightcolour = DARKYELLOW
-    for x3doffset, y3doffset, colour in ((1, 1, darkcolour), (-1, -1, lightcolour), (0, 0, maincolour) if beveled else (0, 0, maincolour)):
-        xoffset = x3doffset
-        yoffset = y3doffset + region.height-(size*1.25)                   # Clunky - FIXME
-        textbitmap = font.render(string, True, colour)
-        if xcentered:
-            xoffset = x3doffset + (region.width - textbitmap.get_width())/2
-        if ycentered:
-            yoffset = y3doffset + (region.height - textbitmap.get_height())/2
-        outputbitmap.blit(textbitmap, (xoffset, yoffset))
-    drawsurface.blit(outputbitmap, region)
+defaultSize = 12
+defaultColour = YELLOW
+defaultFont = "./fonts/MorrisRomanBlack.ttf" #'Dejavu Sans'
 
+def draw(drawSurface, textString, rect, colour=defaultColour, size=defaultSize, font=defaultFont, xcentered=True, ycentered=True, beveled=False):
+    outputBitmap = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA, 32)
+    font = pygame.font.Font(font, size)
+    maincolour = pygame.Color(colour[0], colour[1], colour[2])
+    darkcolour = BLACK # maincolour.correct_gamma(10)
+    lightcolour = WHITE # maincolour.correct_gamma(0.1)
+    for (xbeveloffset, ybeveloffset, colour) in ((1, 1, darkcolour), (-1, -1, lightcolour), (0, 0, maincolour)) if beveled else ((0, 0, colour),):
+        xoffset = xbeveloffset
+        yoffset = ybeveloffset + rect.height-size                   # Clunky - FIXME
+        textBitmap = font.render(textString, True, colour)
+        if xcentered:
+            xoffset = xbeveloffset + (rect.width - textBitmap.get_width())/2
+        if ycentered:
+            yoffset = ybeveloffset + (rect.height - textBitmap.get_height())/2
+        
+        outputBitmap.blit(textBitmap, (xoffset, yoffset))
+
+    drawSurface.blit(outputBitmap, rect.topleft)
+    
