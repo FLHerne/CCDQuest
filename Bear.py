a031a5c65ccb8f760716561b10ef87daeb5570af
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-14 22:15:41 +0100

    Sane variable naming (again).

--- Bear.py
+++ Bear.py
@@ -22,38 +22,38 @@ class Bear:
         if (mindist(playerpos[0], self.position[0], cellmap.size[0])**2 +
             mindist(playerpos[1], self.position[1], cellmap.size[1])**2) > self.detectionrange**2:
             return False
-        def mapcoord(d_coord):
+        def mapcoord(pfcoord):
             '''get map coordinate from pathfinder ones'''
-            return ((self.position[0] + d_coord[0] - self.pfmapsize) % cellmap.size[0],
-                    (self.position[1] + d_coord[1] - self.pfmapsize) % cellmap.size[1])
+            return ((self.position[0] + pfcoord[0] - self.pfmapsize) % cellmap.size[0],
+                    (self.position[1] + pfcoord[1] - self.pfmapsize) % cellmap.size[1])
 
         foundtarget = False
         dijkstramap = [[(self.pfinitialvalue, (self.pfmapsize, self.pfmapsize)) for x in xrange(2*self.pfmapsize)] for x in xrange(2*self.pfmapsize)]
         import heapq
         openlist = []
         heapq.heappush(openlist, (0, (self.pfmapsize, self.pfmapsize)))
-        curp = False
+        curpos = None
         while openlist:
-            curn = heapq.heappop(openlist)
-            curd = curn[0]
-            curp = curn[1]
-            if mapcoord(curp) == tuple(playerpos):
+            curnode = heapq.heappop(openlist)
+            curdist = curnode[0]
+            curpos = curnode[1]
+            if mapcoord(curpos) == tuple(playerpos):
                 foundtarget = True
                 break
-            for nbrpos in [(curp[0]-1, curp[1]), (curp[0], curp[1]-1), (curp[0]+1, curp[1]), (curp[0], curp[1]+1)]:
+            for nbrpos in [(curpos[0]-1, curpos[1]), (curpos[0], curpos[1]-1), (curpos[0]+1, curpos[1]), (curpos[0], curpos[1]+1)]:
                 if nbrpos[0] < 0 or nbrpos[1] < 0 or nbrpos[0] >= 2*self.pfmapsize or nbrpos[1] >= 2*self.pfmapsize:
                     continue
                 if dijkstramap[nbrpos[0]][nbrpos[1]][0] != self.pfinitialvalue or cellmap[mapcoord(nbrpos)].solid:
                     continue
-                dijkstramap[nbrpos[0]][nbrpos[1]] = (curd+1, curp)
-                heapq.heappush(openlist, (curd+1, nbrpos))
+                dijkstramap[nbrpos[0]][nbrpos[1]] = (curdist+1, curpos)
+                heapq.heappush(openlist, (curdist+1, nbrpos))
         if not foundtarget:
             return False
-        while dijkstramap[curp[0]][curp[1]][1] != (self.pfmapsize, self.pfmapsize):
-            curp = dijkstramap[curp[0]][curp[1]][1]
-        self.position[0] = (self.position[0]+curp[0]-self.pfmapsize)%cellmap.size[0]
-        self.direction = curp[0]-self.pfmapsize if abs(curp[0]-self.pfmapsize) else self.direction
-        self.position[1] = (self.position[1]+curp[1]-self.pfmapsize)%cellmap.size[1]
+        while dijkstramap[curpos[0]][curpos[1]][1] != (self.pfmapsize, self.pfmapsize):
+            curpos = dijkstramap[curpos[0]][curpos[1]][1]
+        self.position[0] = (self.position[0]+curpos[0]-self.pfmapsize)%cellmap.size[0]
+        self.direction = curpos[0]-self.pfmapsize if abs(curpos[0]-self.pfmapsize) else self.direction
+        self.position[1] = (self.position[1]+curpos[1]-self.pfmapsize)%cellmap.size[1]
         return True
 
     def sprite(self):
