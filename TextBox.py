b75d169e4f9e2ccce2aaa2e74635e8f2fd3e9a9e
Christopher Stone <cedstone@lithoi.org.uk>
2014-09-19 20:23:55 +0100

    Tweaks to text appearance

--- TextBox.py
+++ TextBox.py
@@ -7,9 +7,10 @@ def draw(drawsurface, string, region, size, color, font=defaultfont, xcentered=T
     outputbitmap = pygame.Surface((region.width, region.height), pygame.SRCALPHA, 32)
     font = pygame.font.Font(font, size)
     maincolor = pygame.Color(color[0], color[1], color[2])
-    darkcolor = maincolor.correct_gamma(10)
-    lightcolor = BLACK # maincolor.correct_gamma(0.1)
-    for (xbeveloffset, ybeveloffset, color) in ((1, 1, darkcolor), (1, -1, darkcolor), (-1, 1, darkcolor), (-1, -1, lightcolor), (0, 0, maincolor)) if beveled else ((0, 0, color),):
+    maincolor = maincolor.correct_gamma(1) 
+    darkcolor = BLACK # maincolor.correct_gamma(8)
+    lightcolor = maincolor.correct_gamma(8)
+    for (xbeveloffset, ybeveloffset, color) in ((1, 1, darkcolor), (1, -1, lightcolor), (-1, 1, darkcolor), (-1, -1, lightcolor), (0, 0, maincolor)) if beveled else ((0, 0, color),):
         xoffset = xbeveloffset
         yoffset = ybeveloffset + region.height-size                   # Clunky - FIXME
         textbitmap = font.render(string, True, color)
