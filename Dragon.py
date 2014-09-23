e61a6740a88bfa4bde5613514584e3a91ba7bc92
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-23 22:02:12 +0100

    Make dragons swarm the player, ridiculously.

--- Dragon.py
+++ Dragon.py
@@ -17,6 +17,25 @@ class Dragon:
 
     def move(self, playerpos, cellmap):
         '''fly around the place'''
+        def tileoffset(a, b, size):
+            subtract = b - a
+            absubtract = abs(subtract)
+            if absubtract*2 <= size:
+                    return subtract
+            else:
+                    return (size-absubtract) * cmp(0, subtract)
+
+        newdirection = [0, 0]
+        offsetx = tileoffset(self.position[0], playerpos[0], cellmap.size[0])
+        offsety = tileoffset(self.position[1], playerpos[1], cellmap.size[1])
+        if abs(offsety) > abs(offsetx):
+            newdirection[0] = self.direction[0]
+            newdirection[1] = cmp(offsety, 0)
+        else:
+            newdirection[0] = cmp(offsetx, 0)
+            newdirection[1] = self.direction[1]
+        self.direction = tuple(newdirection)
+
         self.position = ((self.position[0]+self.direction[0]) % cellmap.size[0],
                          (self.position[1]+self.direction[1]) % cellmap.size[1])
 
