7f57088d8c0e18fbc6692eade9e5c07136d06836
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-23 23:45:07 +0100

    Make dragons behave less obsessively.

--- Dragon.py
+++ Dragon.py
@@ -5,6 +5,7 @@ from directions import *
 class Dragon:
     '''follows you around when in range'''
     detectionrange = 18
+    speed = 0.8
 
     def __init__(self, position):
         '''setup bear in given position'''
@@ -24,7 +25,7 @@ class Dragon:
                     offset[axis] = (size-absubtract) * cmp(0, subtract)
             return offset
 
-        if not cellmap[playerpos].top:
+        if not cellmap[playerpos].top and random.random() < Dragon.speed:
             offset = tileoffset(self.position, playerpos, cellmap.size)
             if offset[0]**2 + offset[1]**1 <= Dragon.detectionrange**2:
                 newdirection = list(self.direction)
