2360b06cc2bf7e42bfc9906edebe2745702154cc
Francis Herne <francis.herne@yahoo.co.uk>
2014-09-23 23:08:16 +0100

    Make dragons use the right directional sprite.
    Offset is still wrong for all angles not UPLEFT.

--- Dragon.py
+++ Dragon.py
@@ -1,19 +1,13 @@
 import images
 import random
+from directions import *
 
 class Dragon:
     '''follows you around when in range'''
-
-    UPLEFT = (-1,-1)
-    UPRIGHT = (1,-1)
-    DOWNLEFT = (-1,1)
-    DOWNRIGHT = (1,1)
-
     def __init__(self, position):
         '''setup bear in given position'''
         self.position = position
-        self.direction = Dragon.UPLEFT
-        self.speed = 0.9
+        self.direction = UPLEFT
 
     def move(self, playerpos, cellmap):
         '''fly around the place'''
@@ -26,19 +20,17 @@ class Dragon:
                     return (size-absubtract) * cmp(0, subtract)
 
         if not cellmap[playerpos].top:
-            newdirection = [0, 0]
             offsetx = tileoffset(self.position[0], playerpos[0], cellmap.size[0])
             offsety = tileoffset(self.position[1], playerpos[1], cellmap.size[1])
-            if abs(offsety) > abs(offsetx):
-                newdirection[0] = self.direction[0]
-                newdirection[1] = cmp(offsety, 0)
-            else:
+            newdirection = list(self.direction)
+            if abs(offsetx) > abs(offsety):
                 newdirection[0] = cmp(offsetx, 0)
-                newdirection[1] = self.direction[1]
+            elif abs(offsety) > abs(offsetx):
+                newdirection[1] = cmp(offsety, 0)
             self.direction = tuple(newdirection)
 
         self.position = ((self.position[0]+self.direction[0]) % cellmap.size[0],
                          (self.position[1]+self.direction[1]) % cellmap.size[1])
 
     def sprite(self):
-        return images.DragonRedUpLeft
+        return images.DragonRed[self.direction]
