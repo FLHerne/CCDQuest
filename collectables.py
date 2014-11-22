9b6109be3eefa08c302f7981f12e751857fd9b03
Francis Herne <francis.herne@yahoo.co.uk>
2014-11-22 23:38:24 +0000

    Add error-checking for map images.
    Behaved really horribly before when reading invalid maps.

--- collectables.py
+++ collectables.py
@@ -1,22 +1,28 @@
 from colors import *
+import terrain
 
+NONE = 0
 COIN = 1
 CHOCOLATE = 2
 DYNAMITE = 3
 
 mapcolor = {
+    WHITE:  NONE,
     YELLOW: COIN,
     BROWN:  CHOCOLATE,
     RED:    DYNAMITE
 }
+colorlist = [terrain.mapcolor(color) for color in mapcolor.keys()]
 
 value = {
+    NONE: 0,
     COIN: 1,
     CHOCOLATE: 50,
     DYNAMITE: 1
 }
 
 name = {
+    NONE: "",
     COIN: "a coin",
     CHOCOLATE: "some chocolate",
     DYNAMITE: "a stick of dynamite"
